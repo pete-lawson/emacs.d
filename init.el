96f2f896860f96076c8c7b5b48a1c496387a2dde
Pete Lawson <geekypete@gmail.com>
2022-04-27 08:26:33 -0400

    added projectile with ivy completion

--- init.el
+++ init.el
@@ -201,6 +201,7 @@
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
   :bind-keymap
   ("C-c p" . projectile-command-map)
   :init
